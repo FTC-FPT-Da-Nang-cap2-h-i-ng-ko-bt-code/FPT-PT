d76e8b33c2eb28d8b97fca67befc8b8f447b5f2f
BillyVNYT <billy.gaming2012@gmail.com>
2026-02-06 23:32:48 +0700

    Implement reveal animations for team cards and add scroll restoration functionality

--- effectCss.js
+++ effectCss.js
@@ -23,7 +23,31 @@ window.addEventListener("load", () => {
     });
 
 });
+window.addEventListener("load", () => {
 
+    const observer = new IntersectionObserver((entries) => {
+
+        entries.forEach((entry) => {
+
+            if(entry.isIntersecting){
+                // vào màn hình -> hiện
+                entry.target.classList.add("show2");
+            }else{
+                // ra khỏi màn hình -> ẩn lại
+                entry.target.classList.remove("show2");
+            }
+
+        });
+
+    }, {
+        threshold: 0.2
+    });
+
+    document.querySelectorAll(".reveal2").forEach(el=>{
+        observer.observe(el);
+    });
+
+});
 document.querySelectorAll(".hover-text").forEach(el => {
 
     const words = el.textContent.split(" ");
@@ -38,4 +62,36 @@ document.querySelectorAll(".hover-text").forEach(el => {
 
     });
 
+});
+window.addEventListener("load", () => {
+
+    const observer = new IntersectionObserver((entries) => {
+
+        entries.forEach((entry) => {
+
+            if(entry.isIntersecting){
+                // vào màn hình -> hiện
+                entry.target.classList.add("show3");
+            }else{
+                // ra khỏi màn hình -> ẩn lại
+                entry.target.classList.remove("show3");
+            }
+
+        });
+
+    }, {
+        threshold: 0.2
+    });
+
+    document.querySelectorAll(".reveal3").forEach(el=>{
+        observer.observe(el);
+    });
+
+});
+if ("scrollRestoration" in history) {
+    history.scrollRestoration = "manual";
+}
+
+window.addEventListener("load", () => {
+    window.scrollTo(0,0);
 });
